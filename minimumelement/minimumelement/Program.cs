cb15073b7d83726231b5ad84f11aae0d9d16a728
wyrenside <haciyev004@icloud.com>
2025-03-16 21:57:29 +0400

    the way for finding minimum element of the given array

---
+++ minimumelement/minimumelement/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace minimumelement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array = {21,38,23,17,19,44,64};
+
+            int minElement = minnimum.Min(Array);
+
+            Console.WriteLine("Smallest number of the given array is: " + minElement);
+        }
+    }
+}
---
+++ minimumelement/minimumelement/minnimum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minimumelement
+{
+    class minnimum
+    {
+        public static int Min(int[] numbers)
+        {
+            int min = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+            }
+            return min;
+        }
+    }
+}
